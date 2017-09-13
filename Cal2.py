031bd1937a451c943ec6666dc66c30e051e9c0fc
piyushghate <piyushghate17@gmail.com>
2017-09-13 05:53:08 +0100

    add,sub,multi,div
    This can perform addition, sub, multi and div of two numbers

--- Cal2.py
+++ Cal2.py
@@ -10,6 +10,10 @@ def multi(x,y):
     z = int(x) * int(y)
     print(x+' * '+y+' = '+ str(z))
 
+def div(x,y):
+    z = int(x) / int(y)
+    print(x+' * '+y+' = '+str(z))
+
 print('Enter the two values :')
 x = input()
 y = input()
@@ -17,3 +21,4 @@ y = input()
 add(x,y)
 sub(x,y)
 multi(x,y)
+div(x,y)
