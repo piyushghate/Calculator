b1c96c80fe86321d51e28e069d0f072d8ba7a2aa
piyushghate <piyushghate17@gmail.com>
2017-09-13 05:50:20 +0100

    Add files via upload

--- Cal2.py
+++ Cal2.py
@@ -6,9 +6,14 @@ def sub(x,y):
     z = int(x) - int(y)
     print(x+' - '+y+' = '+ str(z))
 
+def multi(x,y):
+    z = int(x) * int(y)
+    print(x+' * '+y+' = '+ str(z))
+
 print('Enter the two values :')
 x = input()
 y = input()
 
 add(x,y)
 sub(x,y)
+multi(x,y)
